f01c461a24c84f6447363e2aff8149bbd1d1642e
rittikbasu <irittik@gmail.com>
2022-07-16 21:12:01 +0530

    finished populating cards with local data using props

--- src/App.js
+++ src/App.js
@@ -1,33 +1,58 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
-// import Card from "./components/Card"
-// import Footer from "./components/Footer"
+import Card from "./components/Card"
+import data from "./data"
  
+// let cardArr = []
+// const fetchRandomPerson = async () => {
+//   for (let i = 0; i < 3; i++) {
+//     let response = await fetch("https://randomuser.me/api/")
+//     let data = await response.json()
+//     let rawData = data['results'][0]
+//     let rawName = rawData['name']
+//     let name = rawName['first'] + ' ' + rawName['last']
+//     let title = "Life Lessons with " + name
+//     let picture = rawData['picture']['medium']
+//     let price = rawData['dob']['age']
+//     let reviewCount = rawData['registered']['age']
+//     // cardArr.push(<Card img={picture} title={title} price={price} rating={5} reviewCount={reviewCount} country={rawData['nat']} />)
+//     cardArr.push({img:picture, title:title, price:price, rating:5, reviewCount:reviewCount, country:rawData['nat']})
+//   }
+// }
 
-const fetchRandomPerson = async () => {
-  const response = await fetch("https://randomuser.me/api/")
-  const data = await response.json()
-  let rawData = data['results'][0]
-  let rawName = rawData['name']
-  let name = rawName['first'] + ' ' + rawName['last']
-  let title = "Life Lessons with " + name
-  let picture = rawData['picture']['medium']
-  let price = rawData['dob']['age']
-  let reviewCount = rawData['registered']['age']
-  let filteredData = {img:picture, title:title, price:price, rating:5, reviewCount:reviewCount, country:rawData['nat']}
-  return await filteredData;
-}
 
-console.log(fetchRandomPerson())
+// fetchRandomPerson()  
+
+// const cardElements = cardArr.map(data => {
+//   return (
+//   <Card 
+//   img={data.img} title={data.title} price={data.price} rating={data.rating} reviewCount={data.reviewCount} country={data.country} 
+//   />
+//   )
+// })
 
 function App() {
+  const cards = data.map(item => {
+    return (
+      <Card 
+      img={item.coverImg} 
+      title={item.title} 
+      price={item.price} 
+      rating={item.stats.rating} 
+      reviewCount={item.stats.reviewCount} 
+      country={item.location} 
+      />
+    )
+  })
+
   return (
     <div>
       <Navbar />
       <Hero />
-      {/* <Card props="fetchRandomPerson()"> */}
-
+      <section className="cards-list">
+        {cards}
+      </section>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,18 @@
 import React from "react"
 
 export default function Card(props) {
+    console.log(props)
     return (
         <div className="card">
             <img className="card--image" src={props.img} />
             <div className="card--stats">
-                <img className="card--image" src="https://pngimg.com/uploads/red_star/red_star_PNG31.png" />
+                <img className="card--star" src="https://pngimg.com/uploads/red_star/red_star_PNG31.png" />
                 <span>{props.rating}</span>
-                <span>{props.rating}</span>
-                <span>{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p><span className="bold">From {props.price}</span>/person</p>
+            <p className="card--title">{props.title}</p>
+            <p className="card--price"><span className="bold">From ${props.price}</span> / person</p>
         </div>
         
     )
